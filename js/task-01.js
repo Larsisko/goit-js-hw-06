1eec187540acdb0a538c669ea3a99eb5f5121cca
Larsisko <arkadiusz.miazga1988@gmail.com>
2023-09-03 15:10:48 +0200

    task 10 optional

--- js/task-01.js
+++ js/task-01.js
@@ -6,6 +6,6 @@ categoriesItem.forEach((category) => {
   const categoryName = category.querySelector("h2").textContent;
   const categoryElement = category.querySelectorAll("ul li").length;
 
-  console.log(`Category:` + categoryName);
-  console.log(`Elements:` + categoryElement);
+  console.log(`Category:`, categoryName);
+  console.log(`Elements:`, categoryElement);
 });
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,28 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input[type = "number"]');
+const creatBtn = document.querySelector("button[data-create");
+const removeBtn = document.querySelector("button[data-destroy]");
+const divBox = document.querySelector("#boxes");
+
+creatBtn.addEventListener("click", createBoxes);
+removeBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes() {
+  const amount = input.value;
+  const boxs = [];
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10} px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxs.push(box);
+  }
+  divBox.append(...boxs);
+}
+
+function destroyBoxes() {
+  divBox.innerHTML = "";
+}
